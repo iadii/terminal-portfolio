28fb8ba60e54abb62669d476aabc1c5b81cba537
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-11 23:22:03 +0530

    added scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
   return (
     <div className="min-h-screen bg-terminal-bg flex flex-col md:flex-row md:overflow-auto">
       {/* Left Section - Lanyard (40%) */}
-      <div className="w-full md:w-2/5 flex items-center justify-center p-4 md:p-8 sm:p-2">
+      <div className="w-full md:w-2/5 flex justify-center p-4 md:p-8 sm:p-2 items-start">
         <Lanyard />
       </div>
       {/* Right Section - Terminal (60%) */}
-      <div className="w-full md:w-3/5 p-2 md:p-4 sm:p-1 flex items-center justify-center">
-        <div className="w-full">
+      <div className="w-full md:w-3/5 p-2 md:p-4 sm:p-1 flex justify-center items-start">
+        <div className="w-full max-w-[98vw] sm:max-w-[95vw] md:max-w-full">
           <Terminal />
         </div>
       </div>
