a9534cb7a28a0f347d44cd3a2274efbff6e04606
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-13 09:21:19 +0530

    responsive for tablet

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Terminal from './components/Terminal'
 
 function App() {
   return (
-    <div className="min-h-screen bg-terminal-bg flex flex-col md:flex-row md:overflow-auto">
+    <div className="main-flex-layout min-h-screen bg-terminal-bg flex flex-col md:flex-row md:overflow-auto">
       {/* Left Section - Lanyard (40%) */}
-      <div className="w-full md:w-2/5 flex justify-center p-4 md:p-8 sm:p-2 items-start">
+      <div className="left-section w-full md:w-2/5 flex justify-center p-4 md:p-8 sm:p-2 items-start">
         <Lanyard />
       </div>
       {/* Right Section - Terminal (60%) */}
-      <div className="w-full md:w-3/5 p-2 md:p-4 sm:p-1 flex justify-center items-start">
+      <div className="right-section w-full md:w-3/5 p-2 md:p-4 sm:p-1 flex justify-center items-start">
         <div className="w-full max-w-[98vw] sm:max-w-[95vw] md:max-w-full">
           <Terminal />
         </div>
